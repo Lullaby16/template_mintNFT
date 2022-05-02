2ddc80d13e983ec0483483d392a0e1fc3931f11e
Lullaby16 <samuelys945@gmail.com>
2022-05-02 10:20:57 +0700

    first commit

--- client/src/components/RoadMap.jsx
+++ client/src/components/RoadMap.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 const RoadMap = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex flex-col snap-start bg-cyan-300 font-vt323 w-screen h-screen">
       <div className="text-center text-5xl mt-5">
@@ -8,37 +11,100 @@ const RoadMap = () => {
       </div>
       <div className="flex flex-col w-full h-full justify-between items-center p-10">
         <div className="w-full flex text-black p-5 justify-between">
-          <div className="w-60 h-40 border-2 border-black rounded text-center">
-            <p>White List Sale</p>
-          </div>
-          <div className="w-60 h-40 border-2 border-black rounded text-center">
-            <p>Open Beta</p>
-          </div>
-          <div className="w-60 h-40 border-2 border-black rounded text-center">
-            <p>Sales</p>
-          </div>
+          <motion.div
+            onClick={() => setIsOpen(!isOpen)}
+            className={`w-60 ${
+              isOpen === "true" ? "h-20" : "h-60"
+            } border-2 border-black rounded p-5`}
+          >
+            <motion.p className="text-center">White List Sale</motion.p>
+            {isOpen && (
+              <motion.div className="mt-3">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </motion.div>
+            )}
+          </motion.div>
+          <motion.div className="w-60 h-60 border-2 border-black rounded p-5">
+            <motion.p className="text-center">Open Beta</motion.p>
+            <motion.div className="mt-3">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </motion.div>
+          </motion.div>
+          <motion.div className="w-60 h-60 border-2 border-black rounded p-5">
+            <motion.p className="text-center">Sales</motion.p>
+            <motion.div className="mt-3">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </motion.div>
+          </motion.div>
         </div>
+
         <div className="w-full flex text-black p-5 justify-between">
-          <div className="w-60 h-40 border-2 border-black rounded text-center">
-            <p>Season 1</p>
-          </div>
-          <div className="w-60 h-40 border-2 border-black rounded text-center">
-            <p>Sales</p>
-          </div>
-          <div className="w-60 h-40 border-2 border-black rounded text-center">
-            <p>Preseason</p>
-          </div>
+          <motion.div className="w-60 h-40 border-2 border-black rounded text-center">
+            <motion.p>Season 1</motion.p>
+            <motion.div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </motion.div>
+          </motion.div>
+          <motion.div className="w-60 h-40 border-2 border-black rounded text-center">
+            <motion.p>Sales</motion.p>
+            <motion.div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </motion.div>
+          </motion.div>
+          <motion.div className="w-60 h-40 border-2 border-black rounded text-center">
+            <motion.p>Preseason</motion.p>
+            <motion.div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </motion.div>
+          </motion.div>
         </div>
+
         <div className="w-full flex text-black p-5 justify-between">
-          <div className="w-60 h-40 border-2 border-black rounded text-center">
-            <p>Feedback</p>
-          </div>
-          <div className="w-60 h-40 border-2 border-black rounded text-center">
-            <p>Reward</p>
-          </div>
-          <div className="w-60 h-40 border-2 border-black rounded text-center">
-            <p>Update Quality Life</p>
-          </div>
+          <motion.div className="w-60 h-40 border-2 border-black rounded text-center">
+            <motion.p>Feedback</motion.p>
+            <motion.div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </motion.div>
+          </motion.div>
+          <motion.div className="w-60 h-40 border-2 border-black rounded text-center">
+            <motion.p>Reward</motion.p>
+            <motion.div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </motion.div>
+          </motion.div>
+          <motion.div className="w-60 h-40 border-2 border-black rounded text-center">
+            <motion.p>Update Quality Life</motion.p>
+            <motion.div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </motion.div>
+          </motion.div>
         </div>
       </div>
     </div>
